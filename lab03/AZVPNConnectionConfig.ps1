5daf7e9c6bd52a577a7872c736c73d45cf9672f8
AZITCAMP <AZITCAMP@outlook.com>
2015-10-05 17:53:21 -0600

    Lab 03 Files

---
+++ lab03/AZVPNConnectionConfig.ps1
@@ -0,0 +1,22 @@
+﻿ 
+ <# 
+.SYNOPSIS 
+   This simple script configure a virtual network gateway connection in Azure. \
+   The script assumes that you have completed the comfiguration of a VPN gateway
+   in Azure, either by deploying the Lab 03 template to create the lab infrasture or
+   by creating the VPN gateway using another method. 
+      
+.DESCRIPTION | USAGE
+   This script assumes that the resource group is named RG-AZITCAMP-LAB02 and that the resource
+   group is located in West US. If this is not true, then change the variable to the appropriate values.
+   This script also assumes that you have configured or will configure the RRAS to use an IKEv2
+   shared key as 'abc123' 
+      
+#> 
+
+ $loc = "West US"
+ $rg = "RG-AZITCAMP-LAB03"
+ $gw = Get-AzureVirtualNetworkGateway -Name vnetgw -ResourceGroupName $rg
+ $ls = Get-AzureLocalNetworkGateway -Name LocalSiteGW -ResourceGroupName $rg
+
+ New-AzureVirtualNetworkGatewayConnection -Name s2svpn -ResourceGroupName $rg -Location $loc -VirtualNetworkGateway1 $gw -LocalNetworkGateway2 $ls -ConnectionType IPsec -RoutingWeight 10 -SharedKey 'abc123'
---
+++ lab03/RRASConfig.ps1
@@ -0,0 +1,29 @@
+﻿# Microsoft Corporation
+# Microsoft Azure Virtual Network
+
+# This configuration sample applies to Microsoft RRAS running on Windows Server 2012.
+# This configuration sample configures an IPSec VPN tunnel used to connect your on-premises VPN device with the Windows Azure Virtual Network gateway.
+
+# !!! In this version, the following support restrictions apply for RRAS:
+# !!! 1. Only IKEv2 is currently supported
+# !!! 2. Only route-based VPN configuration is supported.
+
+# Install RRAS role
+Import-Module ServerManager
+Install-WindowsFeature RemoteAccess -IncludeManagementTools
+Add-WindowsFeature -name Routing -IncludeManagementTools
+
+# !!! NOTE: You may be required to reboot before continuing on with the script.
+
+# Install S2S VPN
+Import-Module RemoteAccess
+Install-RemoteAccess -VpnType VpnS2S
+
+# Add S2S VPN interface
+Add-VpnS2SInterface -Protocol IKEv2 -AuthenticationMethod PSKOnly -NumberOfTries 3 -ResponderAuthenticationMethod PSKOnly -Name <SP_AzureGatewayIpAddress> -Destination <SP_AzureGatewayIpAddress> -IPv4Subnet @("10.0.0.0/16:10") -SharedSecret abc123
+
+# Restart the RRAS service
+Restart-Service RemoteAccess
+
+# Dial-in to Azure gateway (optional)
+#Connect-VpnS2SInterface -Name <SP_AzureGatewayIpAddress> 
