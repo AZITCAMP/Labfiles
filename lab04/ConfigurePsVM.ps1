8f96897ce4ee57feab60d69cc5dffe5e5574c103
AZITCAMP <AZITCAMP@outlook.com>
2015-10-11 09:42:09 -0600

    Lab 04 Files

--- lab04/ConfigurePsVM.ps1
+++ lab04/ConfigurePsVM.ps1
@@ -11,7 +11,7 @@ Configuration ConfigurePsVM
             $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
             Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
             Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-            Stop-Process -Name Explorer 
+            # Stop-Process -Name Explorer 
         
             $disks = Get-PhysicalDisk –CanPool $true
 			New-StoragePool -FriendlyName "DataPool" -StorageSubsystemFriendlyName "Storage Spaces*" -PhysicalDisks $disks | New-VirtualDisk -FriendlyName "DataDisk" -UseMaximumSize -NumberOfColumns $disks.Count -ResiliencySettingName "Simple" -ProvisioningType Fixed -Interleave 65536 | Initialize-Disk -Confirm:$False -PassThru | New-Partition -DriveLetter H –UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel "DATA" -Confirm:$false			
--- lab04/ConfigureSourceVM.ps1
+++ lab04/ConfigureSourceVM.ps1
@@ -11,16 +11,21 @@ Configuration ConfigureSourceVM
         $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
         Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
         Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-        Stop-Process -Name Explorer
+        # Stop-Process -Name Explorer
+        
         $regpath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\'
         $x= get-item -Path $regpath
         $pname = 'LocalAccountTokenFilterPolicy'
         New-ItemProperty $regpath -Name $pname -Value 1 -PropertyType "Dword" 
+        
+        $dir = "c:\Source"
+        New-Item $dir -ItemType directory
         		    
         } 
 
 		TestScript = { 
-			if ($x.GetValue("$pname") -eq $null) {return $false} else {return $true}
+		#	if ($x.GetValue("$pname") -eq $null) {return $false} else {return $true}
+        Test-path c:\source
 		}  
 		GetScript = { <# This must return a hash table #> }          }   
   }
